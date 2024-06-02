429067199b8d6d2767f451a8b5ed5d9d6a851289
Arghya <arghyadas242004@gmail.com>
2024-06-02 14:25:40 +0530

    fix file exec

--- setup.sh
+++ setup.sh
@@ -7,5 +7,5 @@ python setup.py install
 # Navigate back to the original directory
 cd ..
 
-# Install the requirements from requirements.txt
+# Install  the requirements from requirements.txt
 pip install -r requirements.txt
